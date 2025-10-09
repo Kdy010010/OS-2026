3fcb52d94269e53042f5667cf26bc0b37aaeb0f9
KDY <137839917+Kdy010010@users.noreply.github.com>
2025-10-09 19:20:44 +0900

    Add files via upload

---
+++ src/boot.asm
@@ -0,0 +1,12 @@
+section .multiboot
+align 4
+
+MBALIGN  equ 1 << 0
+MEMINFO  equ 1 << 1
+FLAGS    equ MBALIGN | MEMINFO
+MAGIC    equ 0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS)
+
+dd MAGIC
+dd FLAGS
+dd CHECKSUM
